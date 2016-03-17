c13549b3ac23759e8095ea931b8eecd7866564d6
Florian NATALI <florian.natali@france-pari.org>
2016-03-17 14:49:47 +0100

    Structure de base

---
+++ src/Entity/QueueEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florian
+ * Date: 17/03/16
+ * Time: 14:44
+ */
+
+namespace Sportnco\RedisORM\Entity;
+
+
+abstract class QueueEntity
+{
+
+}
---
+++ src/Entity/StorageEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: florian
+ * Date: 17/03/16
+ * Time: 14:42
+ */
+
+namespace Sportnco\RedisORM\Entity;
+
+
+abstract class StorageEntity
+{
+
+}
